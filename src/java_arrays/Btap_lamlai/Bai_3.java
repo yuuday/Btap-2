496560944f27b133f17fbbbe9cd19a2de353f4d7
Spider <qtrinhhong@gmail.com>
2026-09-02 00:22:25 +0700

    Làm lại và phana tích

---
+++ src/java_arrays/Btap_lamlai/Bai_3.java
@@ -0,0 +1,37 @@
+package java_arrays.Btap_lamlai;
+
+public class Bai_3 {
+    static void main(String[] args) {
+
+        int [] array = new int[] {5, 9, 10, 2, 3};
+        int max = array[0];
+        int min = array[0];
+        int indexMax = 0;
+        int indexMin = 0;
+// đoanj trên: ta cho phần tử đầu tiên làm mốc phần tử lớn và phần tử nhỏ nhất
+        // gọi biến index để lưu vị trí của lớn nhất và nhỏ nhất--> ví dụ số array[0] lớn nhất ở vị trí 0
+        for ( int i = 0; i < array.length; i++){
+            if (array[i] > max) {
+                max = array[i];
+                indexMax = i;
+                /*
+                khi array[i] lớn hơn max thì lúc đó max mới bằng sẽ đc cập nhật thành array[i]
+                lúc đó index sẽ cập nhật vị trí của array[i]
+                */
+            }
+
+            if (array[i] < min) {
+                min = array[i];
+                indexMin = i;
+            } // tương tự với tìm max
+        }
+        System.out.printf("Value Max: %s has index %s \n", max, indexMax);
+        System.out.printf("Value Min: %s has index %s", min, indexMin);
+        if (max == min) {
+            System.out.println("Hasn't Min");
+        }else {
+
+        }
+// ở phần điều kiện cuối khi max = min thì sẽ hiển thị không có min
+    }
+}
