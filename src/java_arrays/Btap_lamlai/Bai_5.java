51ffdbff5699017ed1be5c422457425ecbe9950b
Spider <qtrinhhong@gmail.com>
2026-09-03 12:40:59 +0700

    Làm lại và phana tích

--- src/java_arrays/Btap_lamlai/Bai_5.java
+++ src/java_arrays/Btap_lamlai/Bai_5.java
@@ -2,11 +2,11 @@ package java_arrays.Btap_lamlai;
 
 public class Bai_5 {
     static void main(String[] args) {
-        int[] array = new int[] {100, 22, 42, 77, 10, 9};
+        int[] array = new int[] {22, 42, 77,};
         boolean isSorted = true;
 // gọi biến isSorted = true biến này sẽ cho là mảng trên đã được sắp xếp rồi kiểm tra mảng trên nếu đã được sắp xếp thì là true còn chưa được sắp xếp thì biến sẽ đổi thành false
 
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i < array.length - 1; i++) {
             if (array[i] > array[i + 1]) { /* vì sao ở đây là array[i + 1]
             ví dụ phần tử đầu tiên là array[0] = 100 thì array [0 + 1] sẽ là phần tử ở ngay phía sau là 22
             kiểm tra 100 > 22 => không tăng dần
