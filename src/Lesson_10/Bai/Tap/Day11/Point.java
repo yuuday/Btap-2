148cb6b83aa2a8efef86fdd40c70158adb4604ca
Spider <qtrinhhong@gmail.com>
2026-09-10 20:41:57 +0700

    b7

---
+++ src/Lesson_10/Bai/Tap/Day11/Point.java
@@ -0,0 +1,12 @@
+package Lesson_10.Bai.Tap.Day11;
+
+public class Point {
+    public int x;
+    public int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
